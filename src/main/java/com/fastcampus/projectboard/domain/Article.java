b474254622d7542477d8c3c20d2a2cd46c9c9c2f
백귀현 <qorrnlgus95@naver.com>
2023-12-26 19:59:31 +0900

    게피글 도메인

---
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,18 @@
+package com.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+// 게시글
+public class Article {
+
+    private Long id;
+    private String title;
+    private String hashtag;
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+
+}
